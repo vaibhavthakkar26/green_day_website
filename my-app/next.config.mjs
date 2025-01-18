3b71538b55c2db23b102c35f2c45ef488b5c6a22
Vaibhav_Thakkar <vaibhavthakkar26051999@gmail.com>
2025-01-18 19:44:54 +0530

    add env

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        NEXT_APP_API_URL: process.env.NEXT_APP_API_URL,
+        NEXT_X_API_KEY : process.env.NEXT_X_API_KEY,
+        NEXT_TOKEN : process.env.NEXT_TOKEN
+    }
+};
 
 export default nextConfig;
